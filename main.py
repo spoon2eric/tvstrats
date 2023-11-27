a700bb429ff60dff70f4acc29141013348583a08
spoon2eric <spoon2eric@gmail.com>
2023-11-27 15:32:18 -0600

    v0.9.13

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def get_all_dot_tickers_from_file():
     return dot_tickers
 
 
-logging.info("tvstrats, version: v0.9.12")
+logging.info("tvstrats, version: v0.9.13")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=5000)
