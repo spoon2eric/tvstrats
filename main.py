3ae955e7754c78cc1c0efce69d156eb5663fd2b4
spoon2eric <spoon2eric@gmail.com>
2023-11-28 20:14:04 -0600

    ticker updates

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def get_all_dot_tickers_from_file():
     return dot_tickers
 
 
-logging.info("tvstrats, version: v0.9.13")
+logging.info("tvstrats, version: v0.9.14")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=5000)
